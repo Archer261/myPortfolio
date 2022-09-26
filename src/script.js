27dc5ea3c1b112bbb08b1588a94bec5747a59fc5
Joseph Archer <viewtiful.kja@gmail.com>
2022-09-25 22:04:09 -0400

    push

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const hamburgerBtn = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
+const projectCard = document.getElementsByClassName("project-card");
+const projectImg = document.getElementsByClassName("project-img");
+
+function toggle() {
+  navList.classList.toggle("show");
+}
