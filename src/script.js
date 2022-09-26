e54e6df3ec8d2f598aec881934e8fdfa38c37161
Joseph Archer <viewtiful.kja@gmail.com>
2022-09-25 23:02:31 -0400

    added contact form

--- src/script.js
+++ src/script.js
@@ -1,8 +1,25 @@
 const hamburgerBtn = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 const projectCard = document.getElementsByClassName("project-card");
-const projectImg = document.getElementsByClassName("project-img");
+const submit = document.getElementById("submit-btn");
+const inputFields = document.querySelectorAll("input");
+const submitRequest = document.getElementById("submit-request");
 
-function toggle() {
+function toggleNav() {
   navList.classList.toggle("show");
 }
+
+function toggleThanks() {
+  submitRequest.style.display = "flex";
+  for (var i = 0; i < inputFields.length; i++) {
+    if (inputFields[i].value) {
+      inputFields[i].value = "";
+    }
+  }
+  setTimeout(function () {
+    submitRequest.style.display = "none";
+  }, 5000);
+}
+
+hamburgerBtn.addEventListener("click", toggleNav);
+submit.addEventListener("click", toggleThanks);
